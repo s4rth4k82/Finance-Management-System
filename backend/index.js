7ea20bcb785f0ecf4ae00115257787b4c0d9eb72
Sarthak Modhe <sarthakpmodhe0802@gmail.com>
2024-06-20 18:39:28 +0530

    reverting changes

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use('/reports', reportRoutes);
 app.use('/categories', categorytRoutes);
 
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
